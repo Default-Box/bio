9a32762bf63869c4656bd2d5be40324a1ef9d5b2
Default-Box <habtejay@gmail.com>
2022-06-17 19:52:14 +1000

    Create foxyisafurry.js

---
+++ pages/foxyisafurry.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../styles/components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Foxy is a furry</title>
+      </Head>
+      <h1>Foxy is a Furry.</h1>
+      <h2>
+      </h2>
+    </Layout>
+  );
+}
