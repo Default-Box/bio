98a63b631b05fadbdaef5fb82ad509d91b6bb2bc
Default-Box <habtejay@gmail.com>
2022-06-17 20:02:28 +1000

    FoxyFox, you asked for this.

---
+++ pages/kirshxpyro.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../styles/components/layout';
+import Image from 'next/image';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Kirsh x Pyro ♡</title>
+      </Head>
+      <h1>Kirsh x Pyro</h1>
+      <Image
+      src="https://c.tenor.com/yH83KX3RjjEAAAAC/floating-hearts-heart.gif"
+      alt="POV: Kirsh x Pyro"
+      width={500}
+      height={500}
+    />
+    <p>A great couple ♡</p>
+      <h2>
+      </h2>
+    </Layout>
+  );
+}
