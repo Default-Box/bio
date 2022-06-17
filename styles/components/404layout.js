f48be2893d1464150e3d298d6a4eb37e165881c7
Default-Box <habtejay@gmail.com>
2022-06-17 18:12:47 +1000

    fixed, I think..

--- styles/components/404layout.js
+++ styles/components/404layout.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../utils.module.css';
 import Link from 'next/link';
-import Script from 'next/Script'
+import Script from 'next/script'
 
 const name = 'Default Box';
 export const siteTitle = 'About Me | Default Box';
