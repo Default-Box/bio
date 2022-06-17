54603bec611f8829566a34952ed0e277eab9b355
Default-Box <habtejay@gmail.com>
2022-06-17 12:29:14 +1000

    Fixed Stuff
    The host now should be able to run.

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hey There! I'm Default Box a professional idiot and I don't know what I'm doing.</p>
+        <p>👋 Hey There! I'm Default Box, a professional idiot and I don't know what I'm doing.</p>
         <p>
         <a href="https://twitter.com/DefaultBox_">Twitter</a>
         </p>
