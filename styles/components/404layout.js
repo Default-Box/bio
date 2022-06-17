a3c42835498deb11b39acb08e6f289ce7aa024a2
Default-Box <habtejay@gmail.com>
2022-06-17 20:09:03 +1000

    stuff

--- styles/components/404layout.js
+++ styles/components/404layout.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Script from 'next/script'
 
 const name = 'Default Box';
-export const siteTitle = 'About Me | Default Box';
+export const siteTitle = `Default Box's Random Website`;
 
 export default function Layout({ children, home }) {
   return (
@@ -17,12 +17,7 @@ export default function Layout({ children, home }) {
           name="description"
           content="📦"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
